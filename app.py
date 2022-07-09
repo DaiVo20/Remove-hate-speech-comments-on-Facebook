7e360f26046c71edbd58874cdec242b638ddc1c7
DaiVo20 <daivt1805@gmail.com>
2022-07-09 16:47:27 +0700

    abc

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template,jsonify
 app = Flask(__name__)
 
+@app.route('/')
+def dashboard():
+    sum_user = 10
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
